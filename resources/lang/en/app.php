d0c9cf9cd21ea7dca2cefe2d0714a0fa8767391e
ismail taibi <61507239+ismadevjs@users.noreply.github.com>
2025-08-30 10:59:37 +0100

    fd

--- resources/lang/en/app.php
+++ resources/lang/en/app.php
@@ -1,6 +1,9 @@
 <?php
 
 return array (
+  'hero_title' => 'Scan before you fly. Connect when you land',
   'hero_heading' => 'Your eSIM is just one click away',
-  'activate_sim' => 'Activate your sim',
+  'hero_description' => 'Keep your online status always active. With our eSIM, you’ll be connected in just 60 seconds. Scan the QR code we sent to your email, activate data roaming, and you’re ready to go.',
+  'hero_btn' => 'Get your eSim',
+  'hero_bnt2' => 'Check Phone Compatibility',
 );
